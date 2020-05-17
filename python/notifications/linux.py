b2d065bed8bfbe3e7e902649fd7a6294d0831797
thecoder777 <thecoder777.github@gmail.com>
2020-05-17 21:32:05 +0200

    added comments and changed the linux version up

--- python/notifications/linux.py
+++ python/notifications/linux.py
@@ -1,12 +1,19 @@
-from notify2 import Notification
-import time
+""" Sending desktop notifications in Linux ..."""
 
-message = Notification(
-    "Summary",
-    message="Some body text",
-    icon="email_icon.ico"
-)
+# install with 'pip install notify2'
+import notify2, time, os
 
+# initialize notify2 with the name 'Email'
+notify2.init("Email")
+
+# create notification
+message = notify2.Notification(
+    "New email",
+    "Hey, wanna meet up at 8 pm?",
+    icon=os.getcwd() + "/email_icon.ico")
+
+# set urgency (LOW, NORMAL, CRITICAL) (optional)
+message.set_urgency(notify2.URGENCY_CRITICAL)
+
+# send the notification
 message.show()
-time.sleep(5)
-message.close()
--- python/notifications/windows.py
+++ python/notifications/windows.py
@@ -1,9 +1,14 @@
-from win10toast import ToastNotifier
-import time
+"""... and Windows """
 
+# install with 'pip install win10toast'
+from win10toast import ToastNotifier
+
+# create a class instance 'toast'
 toast = ToastNotifier()
+
+# create and send the notification
 toast.show_toast(
-    "New Email",
+    "New email",
     "Hey, wanna meet up at 8 pm?",
     icon_path="email_icon.ico",
-    duration=,)
+    duration=10)
