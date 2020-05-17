252671dddca2e0d7dae0e5ca7330f348bba4c170
TheCoder777 <Thecoder777.github@gmail.com>
2020-05-17 19:08:30 +0200

    added some details

--- python/2d_array/2d_arrays.py
+++ python/2d_array/2d_arrays.py
@@ -1,6 +1,9 @@
+#@thecoder777
 
+""" Building a two-dimensional list / table """
 
-# definition of height (rows), width (columns) and content of the table
+# definition of height (rows), width (columns)
+# and the content the 2D-list will consist of
 
 rows = 2
 columns = 3
@@ -8,16 +11,16 @@ content = 0  # 0 just for example
 
 # building the table
 
-table = [[ content for c in range(columns)] for r in range(rows)]
+table = [[content for c in range(columns)] \
+		for r in range(rows)]
 
 # printing the table in a readable format
 
 for row in table:
     print(row)
 
-
 """
-output:
+Output:
 
 [0, 0, 0]
 [0, 0, 0]
