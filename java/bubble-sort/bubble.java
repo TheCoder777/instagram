5b1aad71316013762c457ce46d4498e021887327
thecoder777 <thecoder777.github@gmail.com>
2020-05-18 15:51:55 +0200

    added java bubble-sort

---
+++ java/bubble-sort/bubble.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+public class bubble {
+    public static double[] sort(double[] toSort) {
+        int c = 0;
+        while(true) {
+            c++;
+            for(int i = 0; i + 1 < toSort.length; i++) {
+                if(toSort[i] > toSort[i + 1]) {
+                    double v = toSort[i];
+                    toSort[i] = toSort[i + 1];
+                    toSort[i + 1] = v;
+                }
+            }
+            if( c >= toSort.length) {break;}
+        }
+        return toSort; // return sorted array
+    }
+
+
+    public static void main(String[] args) {
+        double[] arr = {0.95, 5.78, 4.93, 7.36, 3.25};
+	// unsorted
+        System.out.println(Arrays.toString(arr));
+
+        double[] sorted = sort(arr);
+	// sorted
+        System.out.println(Arrays.toString(arr));
+    }
+}
