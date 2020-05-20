9db7fa61732b34deb2f572da73d362c11bb7a533
thecoder777 <thecoder777.github@gmail.com>
2020-05-20 19:05:45 +0200

    added linux-tools ncdu

---
+++ linux-tools/ncdu.sh
@@ -0,0 +1,15 @@
+#@thecoder777 || Linux tools #1
+
+# Linux tools everyone should know #
+
+#> todays_tool("Linux")
+
+ncdu
+
+"Ncdu is a great command line tool
+to determine which files or folders
+take up how much space. This can be
+very useful, especially if you have
+no graphical UI and want to
+declutter your hard drive from time
+to time."
