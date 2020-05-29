c2e20b87ee4e3e08cf8d6884e2019090cf9840ae
thecoder777 <thecoder777.github@gmail.com>
2020-05-29 14:49:29 +0200

    added windows tools 1

---
+++ windows-tools/Tool_1_netcfg.bat
@@ -0,0 +1,16 @@
+#@thecoder777 || Windows tools #1
+
+# Windows tools everyone should know #
+
+#> todays_tool("Windows")
+
+netcfg
+
+"Netcfg is a tool that lets you control
+all the stuff that has to do with your
+internet connection.
+With 'netcfg -d' for example, you can
+perform a hard-reset on your entire
+WIFI/LAN config.
+Please notice that you have to restart
+your PC after you've done this."
