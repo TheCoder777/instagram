6dbea449d1b86165a99f84c1c09b79980fd4bbe7
thecoder777 <thecoder777.github@gmail.com>
2020-06-06 15:18:46 +0200

    fixed typos and comments

--- python/f-strings/formatted-strings.py
+++ python/f-strings/formatted-strings.py
@@ -1,23 +1,24 @@
 # @thecoder777 || Python
 
-""" Formatted Strings in Python """
+""" Formatted Strings in Python [PEP 498]"""
 
 name = "Bob"
 
-# with '.format()': #
+# with '.format()':
 
 print("Hello, my name is {}!".format(name))
 
-# with f-strings: #
+# with f-strings: 'Literal String Interpolation'
 
 print(f"Hello, my name is {name}!")
 
 """
 Output:
+
 Hello, my name is Bob!
 Hello, my name is Bob!
 """
 
-# In my opinion, F-strings are much more
+# In my opinion, f-strings are much more
 # practical, especially if you have a lot
 # of code, it makes it much more readable.
