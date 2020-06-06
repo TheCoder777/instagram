9b5cb59f0876abf9c9c7d1d865eddaa8de4724fe
thecoder777 <thecoder777.github@gmail.com>
2020-06-06 15:16:13 +0200

    added python f-strings

---
+++ python/f-strings/formatted-strings.py
@@ -0,0 +1,23 @@
+# @thecoder777 || Python
+
+""" Formatted Strings in Python """
+
+name = "Bob"
+
+# with '.format()': #
+
+print("Hello, my name is {}!".format(name))
+
+# with f-strings: #
+
+print(f"Hello, my name is {name}!")
+
+"""
+Output:
+Hello, my name is Bob!
+Hello, my name is Bob!
+"""
+
+# In my opinion, F-strings are much more
+# practical, especially if you have a lot
+# of code, it makes it much more readable.
