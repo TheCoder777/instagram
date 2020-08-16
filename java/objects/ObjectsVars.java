0af550d9f85c1c30f786e1007a267bb15fc823d5
TheCoder777 <thecoder777.github@gmail.com>
2020-08-16 12:25:20 +0200

    added simple java oop example

---
+++ java/objects/ObjectsVars.java
@@ -0,0 +1,18 @@
+public class ObjectsVars {
+
+    public static class Car {
+        int speed;
+        String color;
+
+        public Car(int s, String c) {
+            speed = s;
+            color = c;
+        }
+    }
+
+    public static void main(String[] args) {
+        Car c1 = new Car(100, "green");
+        System.out.println(c1.speed);
+        System.out.println(c1.color);
+    }
+}
